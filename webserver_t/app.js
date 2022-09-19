12061aba62bf323c5547c5202b25a0c2c1b78b63
braverokmc79 <braverokmc79@gmail.com>
2022-09-20 03:31:01 +0900

    30.글목록 출력하기

--- webserver_t/app.js
+++ webserver_t/app.js
@@ -1,23 +1,33 @@
-var http = require('http');
-var fs = require('fs');
-var url = require("url");
+const http = require('http');
+const fs = require('fs');
+const url = require("url");
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var title = queryData.id;
-  var pathname = url.parse(_url, true).pathname;
+const app = http.createServer(function (request, response) {
+  const _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
+  let title = queryData.id;
+  let menuList = "";
+
+
 
   if (pathname === '/') {
 
+    //readdirSync 동기식처리
+    let filelist = fs.readdirSync('../data');
+    filelist.forEach(item => {
+      menuList += `
+                <li><a href="?id=${item}">${item}</a></li>
+            `;
+    })
 
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+
+    fs.readFile(`../data/${queryData.id}`, 'utf-8', function (err, description) {
 
       if (queryData.id === undefined) {
         title = "Welcome";
         description = "Hello, Node.js";
       }
-
       var template = `
                   <!doctype html>
           <html>
@@ -26,11 +36,9 @@ var app = http.createServer(function (request, response) {
             <meta charset="utf-8">
           </head>
           <body>
-            <h1><a href="index.html">WEB</a></h1>
+            <h1><a href="/">WEB</a></h1>
             <ol>
-              <li><a href="?id=HTML">HTML</a></li>
-              <li><a href="?id=CSS">CSS</a></li>
-              <li><a href="?id=JavaScript">JavaScript</a></li>
+              ${menuList}
             </ol>
             <h2>${title}</h2>
             <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">
