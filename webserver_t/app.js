190717e0c520fe528af0fcde5f5112f49d496c44
braverokmc79 <braverokmc79@gmail.com>
2022-09-20 14:50:24 +0900

    45.수정할 정보 전송

--- webserver_t/app.js
+++ webserver_t/app.js
@@ -14,8 +14,7 @@ function templateHTML(title, menuList, body) {
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ol>${menuList}</ol>
-            <a href="/create">create</a>
+            <ol>${menuList}</ol>          
             ${body}
     </body>
     </html>       
@@ -38,10 +37,22 @@ function commonPage(queryData, response, head, body) {
       if (queryData.id === undefined) {
         title = "Welcome";
         description = "Hello, Node.js";
+        body = `<a href="/create">create</a><h2>${title}</h2>${description}`;
+      } else {
+        body = `<a href="/create">create</a>&nbsp;&nbsp;&nbsp;<a href="/update?id=${title}">update</a><h2>${title}</h2>${description}`;
       }
-      body = `<h2>${title}</h2>${description}`;
+
     } else if (head === "create") {
       title = "CREATE";
+
+    } else if (head === "update") {
+      body = `<form action="/update_process" method="post">
+        <p><input type="hidden" name="id" value='${title}' ></p>
+        <p><input type="text" name="title" value='${title}'></p>
+        <p><textarea name="description" style="width:200px; height:150px" >${description}</textarea></p>
+        <p><input type="submit" value="수정하기"></p>
+      </form>`;
+
     }
 
     const template = templateHTML(title, templateList(fs.readdirSync('../data')), body);
@@ -85,7 +96,12 @@ const app = http.createServer(function (request, response) {
 
     });
 
+  } else if (pathname === '/update') {
+    commonPage(queryData, response, "update", null);
 
+  } else if (pathname === '/update_process') {
+    response.writeHead(200);
+    response.end("succes");
 
   } else {
     response.writeHead(400);
