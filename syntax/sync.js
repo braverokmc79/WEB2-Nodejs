b689c94a994b1d6caa707b886b1763e028bde9b0
braverokmc79 <braverokmc79@gmail.com>
2022-09-20 12:33:25 +0900

    37.Nodejs에서 동기와 비동기 (2/2)

---
+++ syntax/sync.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+
+//readFileSync
+// console.log('A');
+// const result = fs.readFileSync('sample.txt', 'utf8');
+// console.log(result);
+// console.log('C');
+
+
+console.log('A');
+fs.readFile('sample.txt', 'utf8', function (err, result) {
+    console.log(result);
+});
+console.log('C');
